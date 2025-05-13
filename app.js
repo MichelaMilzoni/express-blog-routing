6c653f38efa2f8789a85eed976c876eb8157b823
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-05-13 18:32:27 +0200

    aggiornato app.js, cartella routers e cartella data

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const express = require("express");
 const app = express();
 
 // importo dati da config.js
-const { config, posts } = require("./config.js");
+const config = require("./config.js");
+
+// importo dati da cartella routers
+//* posts.js
+const postsRouter = require('./routers/posts.js')
+
+// Servire file statici dalla cartella data/imgs con il prefisso /imgs
+app.use("/imgs", express.static("data/imgs"));
 
 
 // avvio del server mettendolo in ascolto sulla porta definita
-app.listen(config.appPortappPort, () => {
+app.listen(config.appPort, () => {
     console.log(`Server avviato su ${config.appUrl} + ${config.appPort}`);
 })
---
+++ routers/posts.js
@@ -0,0 +1,50 @@
+// importo express
+const express = require("express");
+
+// inizializzo variabile router il cui valore è un'istanza di express()
+const router = express.Router();
+
+// importo i dati dell'array
+const postsData = require('../data/postsData'); 
+
+//definisco le rotte
+//! index
+router.get("/", (req, res) => {
+    res.json({
+        message: "Visualizzo la lista di tutti i posts",
+        data: postsData
+    });
+});
+
+//! show
+router.get("/:id", (req, res) => {
+  const post = postsData.find((p) => p.id === parseInt(req.params.id));
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).send("Post non trovato");
+  }
+});
+
+//! store
+router.post('/', (req, res) => {
+    res.send('Creo un nuovo post');
+});
+
+//! update
+router.put('/:id', (req, res) => {
+    res.send('Sostituisco/modifico interamente un post');
+});
+
+//! modify
+router.patch('/:id', (req, res) => {
+    res.send('Modifico parzialmente un post');
+});
+
+//! destroy
+router.delete('/:id', (req, res) => {
+    res.send('Elimino un post');
+});
+
+module.exports = router;
+
